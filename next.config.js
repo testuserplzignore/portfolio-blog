ca9e7b8432fc1d436bd6fac1259853b96ccf0b07
Robert Morrissey <rbrtmorrissey86@gmail.com>
2020-02-21 22:45:27 -0500

    building site map correctly

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ const contentful = require("./services/contentful")
 const Dotenv = require("dotenv-webpack");
 const path = require("path");
 
+const {generateSitemap} = require("./services/deploy/sitemap")
+
+
+const deleteKey = (obj, key) => {
+  if (!!obj && !!obj[key]) {
+    delete obj[key];
+  }
+  console.log(obj);
+  
+  return obj
+}
+
 module.exports = {
   webpack: config => {
     config.target = "node";
@@ -18,7 +30,9 @@ module.exports = {
     return config;
   },
 
-  exportPathMap: async (defaultPathMap) => {
+  exportPathMap: async (defaultPathMap, {dev}) => {
+    console.log(defaultPathMap);
+    
     const blogPosts = (await contentful.getBlog()).items.map(({sys}) => sys.id);
 
     const blogPostPathMap = blogPosts.reduce((acc, slug) => {
@@ -27,12 +41,20 @@ module.exports = {
       };    
       return acc;
     }, {});
-    
-    delete defaultPathMap["/blog/[slug]"];
-    
-    return {
+
+    const map = {
       ...defaultPathMap,
       ...blogPostPathMap
     };
+
+    if (!dev){
+      delete map["/blog/[slug]"];
+      generateSitemap(map, process.env.PUBLIC_DOMAIN, "./out/");
+    }
+
+
+    
+    return map
   }
+
 };
